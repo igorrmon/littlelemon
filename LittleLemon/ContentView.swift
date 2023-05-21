0ce516121940b7138efef1ae2ac4e28f6c084cd2
Igor Diniz <igor.ramon@binome.com.br>
2023-05-20 22:53:43 -0300

    Initial Commit

---
+++ LittleLemon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LittleLemon
+//
+//  Created by Igor Ramon Fernandes Diniz on 20/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Igor Ramon Fernandes Diniz on 20/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
